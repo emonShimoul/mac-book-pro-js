c69432259979675e082ca563f97e4508a67f36b5
emonShimoul <emonshimoul@gmail.com>
2022-04-26 10:44:13 +0600

    add event listener for memory cost, storage cost and delivery charge

--- js/main.js
+++ js/main.js
@@ -21,3 +21,30 @@ function calculateTotal() {
     finalTotalPrice.innerText = total;
     return finalTotalPrice.innerText;
 }
+
+// eventlistener for memory cost
+document.getElementById('8gb-memory-btn').addEventListener('click', function () {
+    extraCost(0, 'extra-memory-cost');
+});
+document.getElementById('16gb-memory-btn').addEventListener('click', function () {
+    extraCost(180, 'extra-memory-cost');
+});
+// eventlistener for storage cost
+document.getElementById('256gb-ssd-btn').addEventListener('click', function () {
+    extraCost(0, 'extra-storage-cost');
+});
+
+document.getElementById('512gb-ssd-btn').addEventListener('click', function () {
+    extraCost(100, 'extra-storage-cost');
+});
+document.getElementById('1tb-ssd-btn').addEventListener('click', function () {
+    extraCost(180, 'extra-storage-cost');
+});
+
+// eventlistener for delivery charge
+document.getElementById('free-delivery-btn').addEventListener('click', function () {
+    extraCost(0, 'delivery-charge');
+});
+document.getElementById('charged-delivery-btn').addEventListener('click', function () {
+    extraCost(20, 'delivery-charge');
+});
