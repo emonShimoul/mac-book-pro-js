fd70d174a15922e48547a692fcfd669db5f5ef57
emonShimoul <emonshimoul@gmail.com>
2022-04-26 10:31:21 +0600

    calculate the total price

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@ function getCost(cost) {
     const extraCost = document.getElementById(cost).innerText;
     return extraCost;
 }
+
+// calculate total price
+function calculateTotal() {
+    const extramemoryPrice = parseFloat(getCost('extra-memory-cost'));
+    const extraStoragePrice = parseFloat(getCost('extra-storage-cost'));
+    const deliveryCharge = parseFloat(getCost('delivery-charge'));
+    const total = 1299 + extramemoryPrice + extraStoragePrice + deliveryCharge;
+    document.getElementById('price-list-total-price').innerText = total;
+
+    const finalTotalPrice = document.getElementById('final-total-price');
+    finalTotalPrice.innerText = total;
+    return finalTotalPrice.innerText;
+}
